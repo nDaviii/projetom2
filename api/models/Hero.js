fa64690b7bcb972d56ee1038f81e9651157b6886
afonsolelis <afonsolelis@gmail.com>
2024-06-10 10:15:25 -0300

    resolva a sua prova

---
+++ api/models/Hero.js
@@ -0,0 +1,20 @@
+module.exports = {
+  attributes: {
+    name: {
+      type: "",
+      required: true,
+    },
+    power: {
+      type: "string",
+      required: true,
+    },
+    age: {
+      type: "integer",
+      required: true,
+    },
+    secretIdentity: {
+      type: "string",
+      required: true,
+    },
+  },
+};
---
+++ config/datastores.js
@@ -0,0 +1,4 @@
+module.exports.datastores = {
+  default: {
+  },
+};
